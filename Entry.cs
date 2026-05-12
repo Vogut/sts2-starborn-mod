84f4045f873e89e59039bcc243a1f9d4ce4761f8
Vogut <398256221@qq.com>
2026-05-12 22:12:40 +0800

    fix:引入ritsulib,初始化

--- Entry.cs
+++ Entry.cs
@@ -1,23 +1,26 @@
-using Godot.Bridge;
-using HarmonyLib;
+using System.Reflection;
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
+using STS2RitsuLib;
+using STS2RitsuLib.Interop;
 
-namespace AzurPromilia.Scripts;
+namespace Starborn.Scripts;
 
 // 必须要加的属性，用于注册Mod。字符串和初始化函数命名一致。
 [ModInitializer(nameof(Init))]
 public class Entry
 {
+    public const string ModId = "Starborn";
+    public static readonly Logger Logger = RitsuLibFramework.CreateLogger(ModId);    
     // 初始化函数
     public static void Init()
     {
-        // 打patch（即修改游戏代码的功能）用
-        // 传入参数随意，只要不和其他人撞车即可
-        var harmony = new Harmony("sts2.reme.testmod");
-        harmony.PatchAll();
-        // 使得tscn可以加载自定义脚本
-        ScriptManagerBridge.LookupScriptsInAssembly(typeof(Entry).Assembly);
-        Log.Info("Mod initialized!");
+        // harmony可用，但是最好用ritsu的封装patch（TODO）
+        // var harmony = new Harmony("com.example.testmod");
+        // harmony.PatchAll();
+        var assembly = Assembly.GetExecutingAssembly();
+        RitsuLibFramework.EnsureGodotScriptsRegistered(assembly, Logger);
+        // 自动注册内容
+        ModTypeDiscoveryHub.RegisterModAssembly(ModId, assembly);
     }
 }
