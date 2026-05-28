8768a731a7889e8258daa5478440c6fad2113550
Vogut <398256221@qq.com>
2026-05-28 16:09:38 +0800

    feat(Cards):添加卡牌 对决指令

---
+++ Cards/Common/DuelCommandCard.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2_Starborn.Character;
+using STS2_Starborn.Commands;
+using STS2_Starborn.Powers;
+
+namespace STS2_Starborn.Cards.Common;
+
+[RegisterCard(typeof(StarbornCardPool))]
+public sealed class DuelCommandCard() : StarbornCard(
+    1, CardType.Skill, CardRarity.Common, TargetType.None
+)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new IntVar("Empower", 5),
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var stacks = DynamicVars["Empower"].IntValue;
+        await PowerCmd.Apply<TemporaryKiboEmpowermentPower>(
+            choiceContext, Owner.Creature, stacks, Owner.Creature, this);
+        await KiboCmd.TryAutoPlayRandomUltimateCard(Owner, Owner.Creature.CombatState!);
+    }
+
+    protected override void OnUpgrade()
+        => DynamicVars["Empower"].BaseValue = 10;
+}
---
+++ Powers/KiboEmpowermentPower.cs
@@ -0,0 +1,59 @@
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Keywords;
+using STS2RitsuLib.Scaffolding.Content;
+using STS2_Starborn.Cards.Kibo;
+
+namespace STS2_Starborn.Powers;
+
+[RegisterPower]
+public class KiboEmpowermentPower : StarbornPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override bool AllowNegative => true;
+
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: Const.Paths.PowerIcon(GetType()),
+        BigIconPath: Const.Paths.PowerBigIcon(GetType())
+    );
+
+    public override decimal ModifyDamageMultiplicative(
+        Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        if (dealer != Owner)
+            return 1m;
+        if (cardSource == null)
+            return 1m;
+        if (!cardSource.HasModKeyword(KiboKeywords.PileMemberKeywordId))
+            return 1m;
+        return 1m + (Amount / 10m);
+    }
+
+    public override decimal ModifyBlockMultiplicative(
+        Creature target, decimal block, ValueProp props, CardModel? cardSource, CardPlay? cardPlay)
+    {
+        if (cardSource?.Owner?.Creature != Owner)
+            return 1m;
+        if (!cardSource.HasModKeyword(KiboKeywords.PileMemberKeywordId))
+            return 1m;
+        return 1m + (Amount / 10m);
+    }
+
+    public override Task AfterModifyingDamageAmount(CardModel? cardSource)
+    {
+        Flash();
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterModifyingBlockAmount(decimal modifiedAmount, CardModel? cardSource, CardPlay? cardPlay)
+    {
+        Flash();
+        return Task.CompletedTask;
+    }
+}
